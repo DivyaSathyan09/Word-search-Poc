3705cf71102f92bdd51b2fbfe001de9a768f3065
DivyaSathyan09 <divya.sathyan@theelixrlabs.com>
2022-12-07 17:06:31 +0530

    reading the file and searching the word in the file.

---
+++ src/main/java/org/example/OccurrenceWordPoc.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class OccurrenceWordPoc {
+    static String inputFilePath;
+    static String searchWord;
+
+    /*
+    Entering the path
+    once if the input is received it shows processing
+    and executes if the file is present or not present
+     */
+    public static void main(String[] args) {
+        inputFilePath = args[0];
+        searchWord = args[1];
+        System.out.println(inputFilePath);
+        File inputFile = new File(inputFilePath);
+        System.out.println("Processing......");
+        if (inputFile.exists()) {
+            System.out.println("File already exists");
+        } else {
+            System.out.println("File dont exist");
+            return;
+        }
+        performRead();
+    }
+
+    /*
+    In this performRead it is used to read the content present in the file
+    by creation of fileReader object and bufferReader object
+    then by using try and catch Exception, reading the content from the file
+    calling performsearch()
+     */
+    public static void performRead() {
+        StringBuilder fileContents = new StringBuilder();
+
+        try {
+            FileReader find = new FileReader(inputFilePath);//Creation of fileReader object
+            BufferedReader bufferedReader = new BufferedReader(find);// Creation of BufferedReader object
+            //Reading content from the file
+            String lineText;
+            while ((lineText = bufferedReader.readLine()) != null) {
+                fileContents.append(lineText);
+            }
+            bufferedReader.close();//Close the file
+        } catch (IOException ex) {
+            System.out.println(" Something went wrong!The file name is Incorrect,The file '" + inputFilePath + "' does not exist.");
+        }
+        // calling performSearch();
+        PerformWord performWord = new PerformWord(inputFilePath, searchWord);
+        performWord.run();
+    }
+}
---
+++ src/main/java/org/example/PerformWord.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+/*
+In this performWord it is used to search the given word present in file
+count the number of ccurance of word and pass the information to main thread
+by using implements Runnable and overriding the run()
+ */
+public class PerformWord implements Runnable {
+    private final String inputFilePath;
+    private final String searchWord;
+
+    public PerformWord(String inputFilePath, String searchWord) {
+        this.inputFilePath = inputFilePath;
+        this.searchWord = searchWord;
+    }
+
+    @Override
+    public void run() {
+        int matchingResultsCount = 0;
+        String inputDataFromFile;
+        try {
+            inputDataFromFile = new String(Files.readAllBytes(Paths.get(inputFilePath)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        inputDataFromFile = inputDataFromFile.replaceAll("[^a-zA-Z0-9@-]", "  ");
+        StringTokenizer st = new StringTokenizer(inputDataFromFile);
+        while (st.hasMoreTokens()) {
+            if (searchWord.equalsIgnoreCase(st.nextToken())) {
+                matchingResultsCount++;
+            }
+        }
+        if (matchingResultsCount > 0) {
+            System.out.println("The Word : " + searchWord + " is Found! Count : " + matchingResultsCount);
+        } else {
+            System.out.println("The Word : " + searchWord + " is incorrect! Count : " + matchingResultsCount);
+        }
+    }
+}
